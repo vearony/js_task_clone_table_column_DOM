053e27f04d0c2a682ae92d7476238b60b8a27843
Veronika Vasyliuk <verka0218@gmail.com>
2024-12-04 21:11:25 +0200

    make build

---
+++ index.dd37bddb.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table").rows,r=!0,l=!1,t=void 0;try{for(var o,n=e[Symbol.iterator]();!(r=(o=n.next()).done);r=!0){var a=o.value,c=a.cells[1];if(c){var i=c.cloneNode(!0);a.insertBefore(i,a.cells[a.cells.length-1])}}}catch(e){l=!0,t=e}finally{try{r||null==n.return||n.return()}finally{if(l)throw t}}
+//# sourceMappingURL=index.dd37bddb.js.map
